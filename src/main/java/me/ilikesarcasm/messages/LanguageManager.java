c823cb13534e54676627e5127aefbdfcc4e41cca
ILikeSarcasm <ilikesarcasm01@gmail.com>
2022-10-09 18:07:52 +0200

    Clean dependencies

--- src/main/java/me/ilikesarcasm/messages/LanguageManager.java
+++ src/main/java/me/ilikesarcasm/messages/LanguageManager.java
@@ -3,7 +3,10 @@ package me.ilikesarcasm.messages;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import javax.management.openmbean.InvalidKeyException;
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
