56c9c4ba94e4046a9e16ea2463e70908fc2d2318
ILikeSarcasm <ilikesarcasm01@gmail.com>
2022-11-23 22:01:51 +0100

    Removing loadLanguage from jar

--- src/main/java/io/github/ilikesarcasm/messages/LanguageManager.java
+++ src/main/java/io/github/ilikesarcasm/messages/LanguageManager.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.Map;
 
 public class LanguageManager {
@@ -45,23 +44,6 @@ public class LanguageManager {
      */
     public void loadLanguage(File languageFile) {
         YamlConfiguration languageConfig = YamlConfiguration.loadConfiguration(languageFile);
-        this.messages = languageConfig.getValues(true);
-    }
-
-    /**
-     * Load messages from a language file.
-     * @param languageFileName The language file name
-     * @param classLoader      The class loader of the plugin
-     * @throws FileNotFoundException when the language file can not be open
-     */
-    public void loadLanguage(String languageFileName, ClassLoader classLoader) throws InvalidKeyException, FileNotFoundException {
-        String languageFile = LanguageManager.LANGUAGE_FOLDER + File.separator + languageFileName;
-        InputStream inputStream = classLoader.getResourceAsStream(languageFile);
-        if (inputStream == null) {
-            throw new FileNotFoundException("Couldn't find " + languageFile);
-        }
-        InputStreamReader languageReader = new InputStreamReader(inputStream);
-        YamlConfiguration languageConfig = YamlConfiguration.loadConfiguration(languageReader);
         this.messages = languageConfig.getValues(true);
     }
 
